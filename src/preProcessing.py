97bdd843556cedb6db8e5bbc37d58fc678f18cc2
mariavmuniz <soaresmunizm@gmail.com>
2023-07-25 11:43:02 -0300

    chore(src): add comments

--- src/preProcessing.py
+++ src/preProcessing.py
@@ -6,17 +6,14 @@ import utils as utils
 import distance as dist
 import math 
 
-#* função para ler todos os arquivos dos pacientes em csv 
-
+#* Função para ler todos os arquivos dos pacientes em csv 
 def read_folder():
 
-    #* encontrar todos os arquivos com extensão ".csv" que são armazenados em uma lista chamada csv_folder
+    csv_folder = glob.glob(os.path.join(utils.PATH_FILES, "*csv")) #* cria um vetor com os paths de todos os arquivos .csv contidos na pasta "PATH_FILES"
 
-    csv_folder = glob.glob(os.path.join(utils.PATH_FILES, "*csv"))
+   
 
-    #* faz um for pelos arquivos criando uma lista com o nome deles
-
-    for file in csv_folder:
+    for file in csv_folder: #* percorre o vetor com os paths dos arquivos .csv
         fileName = os.path.basename(file)
 
         #* verificamos se os dados estão separados por ",", os que estão por vírgula foram colocados nesse "FILES_SEP"
@@ -24,14 +21,13 @@ def read_folder():
         if fileName in utils.FILES_SEP:
             df = pd.read_csv(file, sep = ",")
             df.columns = df.columns.str.upper()
-            df = df.iloc[:, 1:]
+            df = df.iloc[:, 1:] #* retirando coluna "Time"
             df, channelsMiss = insert_columns(df)
             df = algorithm(df, channelsMiss, fileName)
         
-        #* arquivos separados por tabulação
         
         else:
-            df = pd.read_csv(file, sep = "\t")
+            df = pd.read_csv(file, sep = "\t") #* arquivos separados por tabulação
             df.columns = df.columns.str.upper()
             df = df.iloc[:, 1:]
             df, channelsMiss = insert_columns(df)
@@ -39,13 +35,11 @@ def read_folder():
           
 def save_file(fileName, nameFile, df):
     df.to_csv(''.join(fileName) + "/" + nameFile, index= False)  
-        
-#* função para encontar os eletrodos faltantes e colocar uma coluna com -1 nessses lugares
-   
 
+#* Função que ordena, cria um vetor com os eletrodos faltantes e insere uma coluna -1 no local 
 def insert_columns(df):
     
-    df = df.reindex(columns=utils.CHANNELS) #* reordenando as colunas com base da ordem do vetor "CHANNELS"
+    df = df.reindex(columns=utils.CHANNELS) #* reordenando as colunas com base da ordem do vetor "CHANNELS" contido em utils
     channelsMiss  = df.columns[df.isna().any()].tolist() #* vetor com os eletrodos faltantes do arquivo 
     channelsMiss.pop(0) #* retirando CZ do vetor de faltantes
     df = df.fillna(-1) #* substituindo os valores NaN por -1 
@@ -53,7 +47,7 @@ def insert_columns(df):
     
     return df, channelsMiss
 
-#* Função que faz a interpolação e salva o novo csv         
+#* Função que realiza a interpolação e salva o novo csv         
 def algorithm(df, channelsMiss, fileName):
     
     dfInterpol = pd.DataFrame(columns=channelsMiss) #* criando um dataframe apenas com os eletrodos que serão interpolados
@@ -64,19 +58,17 @@ def algorithm(df, channelsMiss, fileName):
         distanceChannel = matrixDistance[electrode] 
         
         
-    #* O método interrows() retorna uma tupla contendo o índice da linha e uma série com os valores daquela linha.
+    #* O método interrows() retorna uma tupla contendo o índice e uma série com os valores daquela linha.
         for index, data in df.iterrows():
-            accumuDen = 0
-            accumuNum = 0
+            accumuDen = 0 #* acumulador responsável pelo somatório do denominador
+            accumuNum = 0 #* acumulador responsável pelo somatório do numerador
             
-            #* o método row.items() percorre a série e retorna o nome da coluna e seu valor
+            #* o método items() percorre a série e retorna o rótulo da coluna e seu valor
             for row, channel in zip(data.items(), distanceChannel):
                 column, value = row #* descompactando o retorno de data.items()
-                
-                #* accumuDen é o acumulador responsável pelo somatório do denominador
-                #* channel é o valor da distância do eletrodo alvo para todos os outros eletrodos
+               
                 if value !=-1: #*verificando se não é um eletrodo faltantes
-                    accumuNum += value * math.exp(-channel)
+                    accumuNum += value * math.exp(-channel)  #* channel é o valor da distância do eletrodo faltante para todos os outros eletrodos
                     accumuDen += math.exp(-channel)
                     
             interpol = round(accumuNum/accumuDen, 4)
@@ -87,6 +79,7 @@ def algorithm(df, channelsMiss, fileName):
     df = df.drop(columns=channelsMiss) 
     dfFinal = pd.concat([df, dfInterpol], axis=1) 
     dfFinal = dfFinal.reindex(columns=utils.CHANNELS)
+    dfFinal = dfFinal.iloc[:, 1:] #* retirando a coluna do eletrodo CZ
 
     save_file(utils.PATH_INTERPOL_FILE, fileName, dfFinal)
     
